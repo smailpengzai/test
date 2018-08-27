e47c83c6c9bc533a8b160e0cf47e884f153905c1
luopeng <lzhlpbaby@gmail.com>
2018-08-27 11:33:14 +0800

    add12

--- gittest/20188221023.go
+++ gittest/20188221023.go
@@ -11,3 +11,12 @@ func test1(){
 func test2(){
 	fmt.Println("test!!!")
 }
+func test3(){
+	fmt.Println("test!!!")
+}
+func test4(){
+	fmt.Println("test!!!")
+}
+func test5(){
+	fmt.Println("test!!!")
+}
